9658f342b5ccd5661c3f0d35125edb68964b8edc
bbanner6ytwtwy <122233076+bbanner6ytwtwy@users.noreply.github.com>
2023-03-06 15:40:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require('axios');
 const app = express();
 const port = 5000;
+
+const apiKey = process.env.API_KEY;
+console.log(apiKey);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.get("/", (req, res) => {
@@ -13,7 +18,6 @@ app.post("/", (req, res) => {
     const lname = req.body.Lname;
     const email = req.body.email;
     const password = req.body.password;
-    const apiKey = '49e117f193cc1a25cf0581dc1c355877-us21';
     const listId = 'da1f50fc17';
     const url = `https://us21.api.mailchimp.com/3.0/lists/${listId}/members`;
     const subscriber = {
